a2103e039579cbc7acc0f8ab332e9f0d3daebce1
mrutyunjay <rout.mrutyunjay@gmail.com>
2023-08-17 15:41:20 -0400

    before react cdn commit

---
+++ Jsx lesson/index.js
@@ -0,0 +1,6 @@
+ 
+
+ const heading =document.createElement('h2');
+ heading.textContent="Hello world";
+ heading.className='header';
+ document.getElementById("root").append(heading);
---
+++ Jsx lesson/script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const btn = document.querySelector(".btn"); // Get the button element by its class name
+    
+    // Add a click event listener to the button
+    btn.addEventListener("click", function() {
+        alert("Button clicked!"); // Display an alert when the button is clicked
+    });
+});
